58a80713fe0c718f9141c221db3ce9a8c5207fa4
Park jiwon <pannchat@likelion.org>
2021-05-27 23:14:25 +0900

    callback hell

---
+++ public/es6/part11/6.js
@@ -0,0 +1,14 @@
+const fs = require('a.text', function(err, dataA){
+    if(err) console.error(err);
+    fs.readFile('b.txt', function(err, dataB){
+        if(err) console.error(err);
+        fs.readFile('c.txt', function(err,dataC){
+            if(err) console.error(err);
+                setTimeout(function(){
+                    fs.writeFile('d.txt', dataA + dataB + dataC, function(err){
+                        if(err) console.error(err);
+                    });
+                }, 60 * 1000);
+            });
+        });
+    });
