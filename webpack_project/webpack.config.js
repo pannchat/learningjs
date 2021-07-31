e06d9025da6f9ed85602f8e9f2a47222265a42c9
pannchat <pannchat@likelion.org>
2021-07-31 20:49:01 +0900

    webpack

--- webpack_project/webpack.config.js
+++ webpack_project/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports={
     entry: './src/index.js',
@@ -26,5 +27,11 @@ module.exports={
                 ],
             }
         ]
-    }
+    },
+    plugins : [
+        new HtmlWebpackPlugin({
+            template: './template.html'
+        })
+    ],
+    mode:'none'
 }
